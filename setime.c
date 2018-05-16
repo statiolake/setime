d917955bb2c5ce9b1df6d8d75e03e3fcb5a60171
statiolake <statiolake@gmail.com>
2018-05-17 03:22:42 +0900

    HWND を先に取得するようにする

--- setime.c
+++ setime.c
@@ -3,9 +3,9 @@
 #include <tchar.h>
 #include <assert.h>
 
-HWND get_hwnd();
-int ime_get();
-void ime_set(int on);
+HWND get_immhwnd();
+int ime_get(HWND immhwnd);
+void ime_set(HWND immhwnd, int on);
 
 void usage();
 
@@ -14,32 +14,32 @@ int main(int argc, char *argv[]) {
         usage();
         return 0;
     }
+
+    HWND immhwnd = get_immhwnd();
     if (strcmp(argv[1], "on") == 0) {
-        ime_set(1);
+        ime_set(immhwnd, 1);
     } else if (strcmp(argv[1], "off") == 0) {
-        ime_set(0);
+        ime_set(immhwnd, 0);
     } else if (strcmp(argv[1], "get") == 0) {
-        return ime_get();
+        return ime_get(immhwnd);
     } else {
         usage();
     }
     return 0;
 }
 
-HWND get_hwnd() {
-    return GetForegroundWindow();
+HWND get_immhwnd() {
+    return ImmGetDefaultIMEWnd(GetForegroundWindow());
 }
 
-int ime_get() {
-    HWND hwnd = get_hwnd();
-    return SendMessage(ImmGetDefaultIMEWnd(hwnd), WM_IME_CONTROL, 0x0005, 0);
+int ime_get(HWND immhwnd) {
+    // 0x0005 : IMC_GETOPENSTATUS
+    return SendMessage(immhwnd, WM_IME_CONTROL, 0x0005, 0);
 }
 
-void ime_set(int on) {
-    HWND hwnd = get_hwnd();
-
+void ime_set(HWND immhwnd, int on) {
     // 0x0006 : IMC_SETOPENSTATUS
-    SendMessage(ImmGetDefaultIMEWnd(hwnd), WM_IME_CONTROL, 0x0006, on);
+    SendMessage(immhwnd, WM_IME_CONTROL, 0x0006, on);
 }
 
 void usage() {
